29686e4b7bd42d66e93bf6e8b940e79a870afe10
Scott Wallace <scott@wallace.sh>
2012-05-12 17:02:01 +0100

    Initial check-in of main deployment script.

---
+++ boss.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+import logging
+import cStringIO
+
+class SingleLevelFilter(logging.Filter):
+    """Class to filter out a single log level.  From, http://stackoverflow.com/questions/1383254/logging-streamhandler-and-standard-streams"""
+    def __init__(self, passlevel, reject):
+        self.passlevel = passlevel
+        self.reject = reject
+
+    def filter(self, record):
+        if self.reject:
+            return (record.levelno != self.passlevel)
+        else:
+            return (record.levelno == self.passlevel)
+
+def runFiles(directory):
+    """Function to execute scripts within a directory"""
+
+    # Ensure the directory exists
+    if not os.path.exists(directory):
+        logger.warn("""Directory "{0}" does not exist.""".format(directory))
+        return False
+
+    # Walk the directory structure
+    logger.info(os.path.basename(directory))
+    for dirname, dirs, files in os.walk(directory):
+        # Iterate over the files
+        for file in files:
+            # Build a relative path to the script
+            filepath = os.path.join(directory, file)
+            # Ensure the script is executable
+            if os.access(filepath, os.X_OK):
+                logger.info("\t{0}".format(file))
+                # Launch the script, via a shell, and redirect STDERR to STDOUT
+                script = subprocess.Popen([r'"{0}"'.format(filepath)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+                output, errors = script.communicate()
+
+                # Show the output of the script
+                for line in cStringIO.StringIO(output):
+                    logger.info("\t\t>> {0}".format(line.strip()))
+                logger.info("")
+            else:
+                # Warn about a non-executable script
+                logger.warn("""The script {0} is not executable.  Skipping.""".format(filepath))
+
+    return True
+
+def deploy():
+    # Resolve the path of where BOSS is installed
+    basedir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+    # Resolve the common-scripts directory
+    common_scriptdir = os.path.join(basedir, "common-scripts")
+
+    # Run the files in the common-scripts directory
+    runFiles(common_scriptdir)
+
+if __name__ == "__main__":
+    # Send INFO logging to stdout
+    stdout = logging.StreamHandler(sys.stdout)
+    infofilter = SingleLevelFilter(logging.INFO, False)
+    stdout.addFilter(infofilter)
+
+    # Send all other levels to stderr
+    stderr = logging.StreamHandler(sys.stderr)
+    otherfilter = SingleLevelFilter(logging.INFO, True)
+    stderr.addFilter(otherfilter)
+
+    # Add the filters to the root logger
+    rootlogger = logging.getLogger()
+    rootlogger.addHandler(stdout)
+    rootlogger.addHandler(stderr)
+
+    # Setup main logger
+    logger = logging.getLogger("boss.logger")
+    logger.setLevel(logging.DEBUG)
+    deploy()
