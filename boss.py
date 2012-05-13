e204c1a206e2eeb191cb505d54449c853a67f8ec
Scott Wallace <scott@wallace.sh>
2012-05-13 12:41:01 +0100

    Added remote detokenisation of configuration files.

--- boss.py
+++ boss.py
@@ -15,6 +15,8 @@ except Exception, e:
     logging.error("""BOSS requires "paramiko".  Please install paramiko before attempting to continue.  See, http://www.lag.net/paramiko/""")
     sys.exit(99)
 
+boss_basedir = ""
+
 class SingleLevelFilter(logging.Filter):
     """Class to filter out a single log level.  From, http://stackoverflow.com/questions/1383254/logging-streamhandler-and-standard-streams"""
     def __init__(self, passlevel, reject):
@@ -28,11 +30,15 @@ class SingleLevelFilter(logging.Filter):
             return (record.levelno == self.passlevel)
 
 class IgnoreMissingKeys(paramiko.MissingHostKeyPolicy):
+    """Class to set up a policy to ignote missing SSH host keys."""
     def missing_host_key(self, client, hostname, key):
         return
 
 class BOSSclient():
     """Class for a remote BOSS client."""
+
+    config_dest = "/srv/cfg"
+    tmpdir = "/var/tmp"
 
     def __init__(self, hostname, username):
         # Set up an SSH client and set the key policy to ignore missing keys
@@ -49,33 +55,33 @@ class BOSSclient():
         # Generate a random string to avoid problems/conflicts
         rndstring = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
 
-        self.remotebasedir = "/var/tmp/BOSS-{0}".format(rndstring)
-        self.client.exec_command("mkdir -p {0}".format(self.remotebasedir))
+        self.remote_basedir = os.path.join(self.tmpdir, "BOSS-{0}".format(rndstring))
+        self.client.exec_command("mkdir -p {0}".format(self.remote_basedir))
 
         # Output hostname
         bosslog.info(hostname)
 
-    def execute(self, directory):
-        """Copies a local directory to a remote host and executes the script."""
+    def deploy(self):
+        """Copies a local directory to a remote host and executes the scripts."""
 
-        if not os.path.exists(directory):
-            bosslog.warn("""The "{0}" script directory does not exist.""".format(directory))
+        if not os.path.exists(self.project):
+            bosslog.warn("""The "{0}" script directory does not exist.""".format(self.project))
             return False
 
         # Build the remote path
-        remotedir = os.path.join(self.remotebasedir, os.path.basename(directory))
+        remotedir = os.path.join(self.remote_basedir, os.path.basename(self.project))
 
         # Create an SFTP channel for transferring files
         sftp = self.client.open_sftp()
         sftp.mkdir(remotedir, 493)
 
         # Output the directory name
-        bosslog.info("| {0}".format(os.path.basename(directory)))
-        for dirname, dirs, files in os.walk(directory):
+        bosslog.info("| {0}".format(os.path.basename(remotedir)))
+        for dirname, dirs, files in os.walk(self.project):
             files.sort()
             for file in files:
                 # Build paths to the local and remote files
-                local_file = os.path.join(directory, file)
+                local_file = os.path.join(self.project, file)
                 remote_file = os.path.join(remotedir, file)
 
                 # Check for execute permissions
@@ -102,13 +108,64 @@ class BOSSclient():
 
                 sftp.remove(remote_file)
         sftp.rmdir(remotedir)
-        sftp.rmdir(self.remotebasedir)
+        sftp.close()
+
+    def configure(self, config_dest=None):
+
+        if config_dest is None:
+            config_dest = self.config_dest
+
+        sftp = self.client.open_sftp()
+
+        configroot = os.path.join(self.remote_basedir, ".configure")
+        sftp.mkdir(configroot)
+
+        for dirname, dirs, files in os.walk("templates"):
+            sftp.mkdir(os.path.join(configroot, dirname))
+            for file in files:
+                sftp.put(os.path.join(dirname, file), os.path.join(configroot, dirname, file))
+
+        for dirname, dirs, files in os.walk("conf"):
+            sftp.mkdir(os.path.join(configroot, dirname))
+            for file in files:
+                sftp.put(os.path.join(dirname, file), os.path.join(configroot, dirname, file))
+
+        sftp.put(os.path.join(boss_basedir, "lib", "detoken.py"), os.path.join(configroot, "detoken.py"))
+        sftp.chmod(os.path.join(configroot, "detoken.py"), 0755)
+
+        sftp.mkdir(config_dest)
+        bosslog.debug("{0} -c {1} -t {2} -d {3}".format(os.path.join(configroot, "detoken.py"),
+                                    os.path.join(configroot, "conf", "{0}-{1}.properties".format(self.context, self.environment)),
+                                    os.path.join(configroot, "templates"),
+                                    config_dest
+                                ))
+        stdin, stdout, stderr = self.client.exec_command("{0} -c {1} -t {2} -d {3}".format(os.path.join(configroot, "detoken.py"),
+                                    os.path.join(configroot, "conf", "{0}-{1}.properties".format(self.context, self.environment)),
+                                    os.path.join(configroot, "templates"),
+                                    config_dest
+                                ))
+        for line in stdout:
+            bosslog.info(line.rstrip())
+        for line in stderr:
+            bosslog.warn(line.rstrip())
+
+        sftp.rmdir(configroot)
+        sftp.close()
+
+    def __del__(self):
+        sftp = self.client.open_sftp()
+        sftp.rmdir(self.remote_basedir)
+        sftp.close()
+
+        self.client.close()
 
 def deploy(project, environment, context):
-    # Resolve the path of where BOSS is installed
-    basedir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    """Main deployment loop."""
 
-    configdir = os.path.join(basedir, "conf")
+    # Resolve the path of where BOSS is installed
+    boss_basedir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+    configdir = os.path.join(boss_basedir, "conf")
 
     # Read and parse the main BOSS config file
     configfile = os.path.join(configdir, "boss.conf")
@@ -127,7 +184,7 @@ def deploy(project, environment, context):
     hosts = hostlist.get(project, context)
 
     # Resolve the common-scripts directory
-    common_scriptdir = os.path.join(basedir, "common-scripts")
+    common_scriptdir = os.path.join(boss_basedir, "common-scripts")
 
     # Loop through each of the configured hosts
     for entry in hosts.split(","):
@@ -153,8 +210,16 @@ def deploy(project, environment, context):
         except Exception, e:
             bosslog.error(e)
         else:
-            remotehost.execute(common_scriptdir)
-            remotehost.execute(project)
+            # Pass through the environment, project and context to the client object
+            remotehost.environment = environment
+            remotehost.project = project
+            remotehost.context = context
+
+            remotehost.configure()
+            #remotehost.deploy(common_scriptdir)
+            #remotehost.deploy()
+
+            del remotehost
 
 if __name__ == "__main__":
     # Send INFO logging to stdout
@@ -183,7 +248,7 @@ if __name__ == "__main__":
     parser.add_option("-c", "--context", dest="context", help="The context of the project.")
     (options, args) = parser.parse_args()
 
-    # Ensure all three options are provided
+    # Ensure all three 'options' are provided
     if (not options.project or not options.environment or not options.context):
         bosslog.error(parser.print_help())
         sys.exit(1)
