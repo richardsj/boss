116c74380423ffadb3ac70830b181c7b8c63bf0c
Scott Wallace <scott@wallace.sh>
2012-05-19 17:14:44 +0100

    Changed the configuration system to be simpler and more robust.

--- lib/Boss/server.py
+++ lib/Boss/server.py
@@ -14,71 +14,57 @@ class server():
     varmap = {}
 
     def __init__(self, project, environment, context):
+        self.project = project
+        self.environment = environment
+        self.context = context
 
         # Main config directory
         configdir = os.path.join(Boss.__install__, "conf")
 
         # Read and parse the main BOSS config file
         conf_file = os.path.join(configdir, "boss.conf")
-        bossconf = ConfigParser.ConfigParser()
-        bossconf.read(conf_file)
-
-        # Read and parse the main environment config file
-        env_file = os.path.join(configdir, "{0}.conf".format(environment))
-        envconf = ConfigParser.ConfigParser()
-        envconf.read(env_file)
+        self.bossconf = ConfigParser.ConfigParser()
+        self.bossconf.read(conf_file)
 
         # Read and parse the per-project config file
-        proj_file = os.path.join(Boss.__install__, "projects", project, "project.conf")
-        projconf = ConfigParser.ConfigParser()
-        projconf.read(proj_file)
-
-        # Fetch the default user, if set
-        try:
-            default_user = bossconf.get("BOSS", "default user")
-        except (ConfigParser.NoOptionError, ConfigParser.NoSectionError), e:
-            default_user = os.getlogin()
-
-        # Get a list of the hosts to deploy to
-        try:
-            hosts = envconf.get(project, context)
-        except ConfigParser.NoOptionError, e:
-            raise Exception("There appears to be no such context ({0}) for the environment ({1})".format(context, environment))
+        proj_file = os.path.join(Boss.__install__, "projects", self.project, "project.conf")
+        self.projconf = ConfigParser.ConfigParser()
+        self.projconf.read(proj_file)
 
         # Resolve the common-scripts directory
         self.common_scriptdir = os.path.join(Boss.__install__, "common", "scripts")
 
         # Loop through each of the configured hosts
-        for entry in hosts.split(","):
-            # Try to work out the username and host for each entry
-            item = entry.split("@")
-            try:
-                # Simply split on the '@' symbol
-                hostname = item[1].strip()
-                user = item[0].strip()
-
-                # Resolve the deployment path
-                try:
-                    deploypath = projconf.get("PROJECT", "deploy path")
-                except (ConfigParser.NoOptionError, ConfigParser.NoSectionError), e:
-                    try:
-                        deploypath = bossconf.get("BOSS", "default deploy path")
-                    except (ConfigParser.NoOptionError, ConfigParser.NoSectionError), e:
-                        deploypath = None
-            except IndexError:
-                # '@' symbol missing.  Assume just a hostname.
-                hostname = item[0].strip()
-                # Set the user to the default user, if available
-                user = default_user
-
+        hosts = self.resolve_option(self.context)
+        for hostname in hosts.split(","):
             self.hosts[hostname] = {}
-            self.hosts[hostname]["user"] = user
-            self.hosts[hostname]["path"] = deploypath
+            self.hosts[hostname]["user"] = self.resolve_option("ssh user", default=os.getlogin())
+            self.hosts[hostname]["path"] = self.resolve_option("deploy path")
 
             # Perform the environment variable mappings, if any
             try:
-                for var, value in bossconf.items("VAR MAPPING"):
+                for var, value in self.bossconf.items("VAR MAPPING"):
                     self.varmap[var] = value
             except ConfigParser.NoSectionError:
                 pass
 
+    def resolve_option(self, option, default=None):
+        """
+        Method to resolve a configuration option by checking the per-project configuration first
+        followed by the main BOSS configuration before falling back to the provided default.
+        """
+
+        # Check the per-environment project config
+        try:
+            return self.projconf.get(self.environment, option)
+        except (ConfigParser.NoOptionError, ConfigParser.NoSectionError), e:
+            # Check project-wide config
+            try:
+                return self.projconf.get("PROJECT", option)
+            except (ConfigParser.NoOptionError, ConfigParser.NoSectionError), e:
+                # Check the main BOSS config
+                try:
+                    return self.bossconf.get("BOSS", option)
+                except (ConfigParser.NoOptionError, ConfigParser.NoSectionError), e:
+                    # Fallback to the default
+                    return default
