4b781977c0faef9592f1e43f17c27bf92565fa4f
Scott Wallace <scott@wallace.sh>
2014-09-08 19:03:22 +0100

    More Python coding standard compliance.

--- bin/detoken.py
+++ bin/detoken.py
@@ -17,127 +17,136 @@ import optparse
 import shutil
 import logging
 
-# Set log level
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+def main():
+    """Function to act as the main loop."""
+    # Set log level
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
-# Parse the CLI options
-logging.debug("Parsing commandline options")
-cliopts = optparse.OptionParser()
-cliopts.add_option(
-    "-c",
-    "--config",
-    dest="configfile",
-    help="Configuration properties file that contain TOKEN=VALUE"
-)
-cliopts.add_option(
-    "-t",
-    "--templates",
-    dest="templates",
-    help="Template tree that contain the tokenised files"
-)
-cliopts.add_option(
-    "-d",
-    "--destination",
-    dest="destination",
-    help="Destination directory to write the final configuration tree"
-)
-(options, args) = cliopts.parse_args()
+    # Parse the CLI options
+    logging.debug("Parsing commandline options")
+    cliopts = optparse.OptionParser()
+    cliopts.add_option(
+        "-c",
+        "--config",
+        dest="configfile",
+        help="Configuration properties file that contain TOKEN=VALUE"
+    )
+    cliopts.add_option(
+        "-t",
+        "--templates",
+        dest="templates",
+        help="Template tree that contain the tokenised files"
+    )
+    cliopts.add_option(
+        "-d",
+        "--destination",
+        dest="destination",
+        help="Destination directory to write the final configuration tree"
+    )
+    (options, _) = cliopts.parse_args()
 
-# Ensure the required CLI options are provided
-if None in [options.configfile, options.templates, options.destination]:
-    cliopts.print_help()
-    sys.exit(1)
+    # Ensure the required CLI options are provided
+    if None in [options.configfile, options.templates, options.destination]:
+        cliopts.print_help()
+        sys.exit(1)
 
-# Parse the configuration file into a dictionary
-values = {}
-try:
-    logging.debug("Parsing configuration file: %s", options.configfile)
-    config = open(options.configfile)
-except IOError, errormessage:
-    logging.warning(errormessage)
-
-for line in config:
+    # Parse the configuration file into a dictionary
+    values = {}
     try:
-        (key, val) = line.split('=', 1)
-        values[key] = val.strip()
-        logging.debug(
-            "Parsed key: \"%s\" and value: \"%s\"",
-            key,
-            values[key]
-        )
-    except ValueError:
-        # Ignore "dodgy" lines
-        logging.debug("Could not parse line: %s", line)
+        logging.debug("Parsing configuration file: %s", options.configfile)
+        config = open(options.configfile)
+    except IOError, errormessage:
+        logging.warning(errormessage)
 
-# Traverse the tree
-for dirpath, dirnames, filenames in os.walk(options.templates):
-    # Ignore SVN directories
-    if ".svn" in dirnames:
-        logging.debug("Ignoring .svn directory")
-        dirnames.remove(".svn")
-
-    # Iterate over the files
-    for filename in filenames:
-        logging.debug("Working on %s", filename)
-        # Extract the relative path for use later
-        relativepath = dirpath[(len(options.templates)+1):]
-
-        infile = os.path.join(dirpath, filename)
-        indata = open(infile)
-
-        # Create the directory for the output file
-        target_directory = os.path.join(options.destination, relativepath)
+    for line in config:
         try:
-            os.stat(target_directory)
-        except OSError:
+            (key, val) = line.split('=', 1)
+            values[key] = val.strip()
+            logging.debug(
+                "Parsed key: \"%s\" and value: \"%s\"",
+                key,
+                values[key]
+            )
+        except ValueError:
+            # Ignore "dodgy" lines
+            logging.debug("Could not parse line: %s", line)
+
+    # Traverse the tree
+    for dirpath, dirnames, filenames in os.walk(options.templates):
+        # Ignore SVN directories
+        if ".svn" in dirnames:
+            logging.debug("Ignoring .svn directory")
+            dirnames.remove(".svn")
+
+        # Iterate over the files
+        for filename in filenames:
+            logging.debug("Working on %s", filename)
+            # Extract the relative path for use later
+            relativepath = dirpath[(len(options.templates)+1):]
+
+            infile = os.path.join(dirpath, filename)
+            indata = open(infile)
+
+            # Create the directory for the output file
+            target_directory = os.path.join(options.destination, relativepath)
             try:
-                os.makedirs(target_directory)
+                os.stat(target_directory)
             except OSError:
-                # Ignore mkdir errors
-                logging.warning(
-                    "Could not make directory: %s",
-                    target_directory
-                )
-
-        outfile = os.path.join(options.destination, relativepath, filename)
-        logging.debug("Writing to %s", outfile)
-        try:
-            output = open(outfile, "w")
-        except IOError, errormessage:
-            logging.error("Could not open a file for writing: %s", errormessage)
-            sys.exit(2)
-
-        # Iterate over the input file
-        for line in indata:
-            newline = line
-
-            # Create a loop where tokens keep substituting until the line
-            # doesn't change. Allows for nested tokens.
-            replaced = 1
-            while replaced == 1:
-                replaced = 0
-                # Find all the tokens in a line
-                for token in values.keys():
-                    logging.debug(
-                        "Replacing \"%s\" with \"%s\"", token, values[token]
+                try:
+                    os.makedirs(target_directory)
+                except OSError:
+                    # Ignore mkdir errors
+                    logging.warning(
+                        "Could not make directory: %s",
+                        target_directory
                     )
-                    newline = line.replace("@%s@" % token, values[token])
 
-                    # Compare the lines, if different start the loop for the
-                    # line again
-                    if newline != line:
-                        logging.debug("Line has changed.  Rechecking.")
-                        line = newline
-                        replaced = 1
-                        break
+            outfile = os.path.join(options.destination, relativepath, filename)
+            logging.debug("Writing to %s", outfile)
+            try:
+                output = open(outfile, "w")
+            except IOError, errormessage:
+                logging.error(
+                    "Could not open a file for writing: %s",
+                    errormessage
+                )
+                sys.exit(2)
 
-            # Write the final substituted line to the output file
-            output.write(newline)
+            # Iterate over the input file
+            for line in indata:
+                newline = line
 
-        indata.close()
-        output.close()
+                # Create a loop where tokens keep substituting until the line
+                # doesn't change. Allows for nested tokens.
+                replaced = 1
+                while replaced == 1:
+                    replaced = 0
+                    # Find all the tokens in a line
+                    for token in values.keys():
+                        logging.debug(
+                            "Replacing \"%s\" with \"%s\"", token, values[token]
+                        )
+                        newline = line.replace("@%s@" % token, values[token])
 
-        logging.debug("Copying from %s to %s", infile, outfile)
-        shutil.copymode(infile, outfile)
-sys.exit(0)
+                        # Compare the lines, if different start the loop for the
+                        # line again
+                        if newline != line:
+                            logging.debug("Line has changed.  Rechecking.")
+                            line = newline
+                            replaced = 1
+                            break
+
+                # Write the final substituted line to the output file
+                output.write(newline)
+
+            indata.close()
+            output.close()
+
+            logging.debug("Copying from %s to %s", infile, outfile)
+            shutil.copymode(infile, outfile)
+    return True
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
